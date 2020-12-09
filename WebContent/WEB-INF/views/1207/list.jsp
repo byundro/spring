2b65693711db9cca669c81b978a7aee45a6da829
byundro <40386868+byundro@users.noreply.github.com>
2020-12-09 19:59:40 +0900

    1209 fix

--- WebContent/WEB-INF/views/1207/list.jsp
+++ WebContent/WEB-INF/views/1207/list.jsp
@@ -3,6 +3,8 @@
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 
 <c:forEach var="dto" items="${list}">
-	${dto.num} ${dto.id} ${dto.orgname} ${dto.reg} <br />
+	${dto.num} ${dto.id} 
+	<a href="/spring/file/down.do?file=${dto.savename}">${dto.orgname}</a>
+	${dto.reg} <br />
 	<hr />
 </c:forEach>
--- src/spring/model/FileBean.java
+++ src/spring/model/FileBean.java
@@ -1,12 +1,17 @@
 package spring.model;
 
 import java.io.File;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
+
 import spring.model.service.FileService;
 
 @Controller
@@ -14,7 +19,7 @@ import spring.model.service.FileService;
 public class FileBean {
    
    @Autowired
-   private FileService fileServiceImpl = null;
+   private FileService FileServiceImpl = null;
    
    @RequestMapping("form.do")
    public String form() {
@@ -27,7 +32,7 @@ public class FileBean {
       MultipartFile mf = request.getFile("save");  // 원본파일객체
       String fileName = mf.getOriginalFilename();  // 파일명 
       dto.setOrgname(fileName);  // 파일의 원본이름을 DTO 추가 
-      int num = fileServiceImpl.fileInsert(dto);
+      int num = FileServiceImpl.fileInsert(dto);
       
       // 파일명에서 확장자 찾기 
       String ext = fileName.substring(fileName.lastIndexOf("."));
@@ -37,7 +42,7 @@ public class FileBean {
       dto.setNum(num);
       dto.setSavename(saveName);
       
-      fileServiceImpl.fileUpdate(dto);  // sql Update 진행 
+      FileServiceImpl.fileUpdate(dto);  // sql Update 진행 
       
       
       //String savePath = "D:\\eclipse-workspace\\spring\\WebContent\\save\\";  // 업로드파일 저장경로 
@@ -54,9 +59,18 @@ public class FileBean {
    @RequestMapping("list.do")
    public String list(Model model)throws Exception {
       
-      model.addAttribute("list", fileServiceImpl.selectAll());
+      model.addAttribute("list", FileServiceImpl.selectAll());
       
       return "1207/list";
    }
    
+   @RequestMapping("down.do")
+   public ModelAndView down(HttpServletRequest request , String file ) {
+	   String path = request.getRealPath("save")+"//"+file;
+	   File f = new File(path);
+	   ModelAndView mv = new ModelAndView("download" , "downloadFile" , f);
+	   								// bean id   , parameterName,	   
+	   return mv;   
+   }
+
 }
